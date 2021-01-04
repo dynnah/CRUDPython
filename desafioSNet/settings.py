7a8e2e6292e3033d23f25cbebc090e9df9439f86
dynnah <dynnah_hanna@hotmail.com>
2021-01-04 12:57:25 -0300

    formulario completo

--- desafioSNet/settings.py
+++ desafioSNet/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'prsybs3wma6k*d9+wd-e@m9-#myk2404tc_v-hwhf&*krh!p%9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
